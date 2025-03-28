bc2d72879d08688a17fa65e8c299b08747558c14
Pakasprogramador <168448938+Pakasprogramador@users.noreply.github.com>
2025-03-28 20:07:42 +0000

    Movimento do Cavalo

--- Xadrez.c
+++ Xadrez.c
@@ -4,14 +4,17 @@
 int main(){
 //variaveis criadas e inicializadas :)
 
-int torre,bispo,rainha,lop,lop2,direcao,direcao2;
+int torre,bispo,rainha,lop,lop2,lop3,direcao,direcao2;
 lop = 1;
 lop2 = 1;
+lop3 = 1;
 torre = 5;
 bispo = 5;
 rainha = 8;
 direcao = 1;
 direcao2 = 0;
+
+
 
 //Saida de dados  e logica do jogo
 printf("Bem vindo ao jogo de Xadrez \n\n");
@@ -74,6 +77,60 @@ for(lop = 1; lop < 2; lop++){
     printf("\n\nA Rainha andou %d casas para a Baixo\n\n",rainha);
     }
 }
-printf("\n\nFim do jogo...\n\n",rainha);
+
+//Cavalo 
+printf("\nMovimento do Cavalo:\n\n");
+for(int lop = 1; lop <= 2;lop++){
+
+for(int baixo = 1 ; baixo <= 2; baixo++){
+
+switch(baixo){
+
+case 1:
+printf("Baixo");
+break;
+
+}
+
+
+}
+printf("\n");
+}
+direcao = 1;
+while (lop3 <= 1)
+{
+    
+    while (lop3 <= 1)
+    {
+        
+        switch(direcao){
+
+            case 1:
+            printf("Esquerda");
+            break;
+            
+            }
+            
+    
+    
+    
+    
+    
+    
+        lop3++;
+    }
+    
+
+printf("\n\n\n");
+
+
+
+
+    lop3++;
+}
+
+
+
+printf("\n\nFim do jogo...\n\n");
     return 0;
 }
